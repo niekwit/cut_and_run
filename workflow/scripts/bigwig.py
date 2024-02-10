f7c07862a63bf7950aa4292c73f494bc163bc94a
niekwit <niekwit@gmail.com>
2024-02-10 12:38:17 +0000

    Added effective genome size for each sample

--- workflow/scripts/bigwig.py
+++ workflow/scripts/bigwig.py
@@ -1,5 +1,6 @@
 from snakemake.shell import shell
 import pandas as pd
+import csv
 
 # Input/output files
 bam = snakemake.input["bam"]
@@ -14,77 +15,33 @@ extra = snakemake.params["extra"]
 threads = snakemake.threads
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 sample = snakemake.wildcards["sample"]
+multiqc = snakemake.input["multiqc"]
+egs = snakemake.input["egs"]
 
 # Load scale factors for sample if spike in was applied
 if snakemake.params["apply_spike_in"]:
     scale_factors = snakemake.input["sf"]
     scale_factors = pd.read_csv(scale_factors)
     scale_factor = scale_factors[scale_factors["sample"] == sample]["scale_factor"].values[0]
-    
+    scaling = f"--scaleFactor {scale_factor}"
     # Set normalisation parameter to None 
-    norm = "None"
+    normalisation = ""
     
-else: # Apply no scale factor (i.e. scale factor = 1, set normalisation parameter in config to not None)
+else: # Apply no scale factor (set normalisation parameter in config to not None)
     if norm in ["None", None]:
         print("WARNING: No scale factor applied and no normalisation parameter set.\nThis will result in a bigwig file with raw read counts.")
-    scale_factor = 1
+    scaling = ""
+    normalisation = f"--normalizeUsing {norm}"
 
-# Default effective genome sizes
-default_effective_genome_size = {
-    "dm6": {
-        "50": 125464728,
-        "75": 127324632,
-        "100": 129789873,
-        "150": 129941135,
-        "200": 132509163,
-    },
-    "hg19": {
-        "50": 2685511504,
-        "75": 2736124973,
-        "100": 2776919808,
-        "150": 2827437033,
-        "200": 2855464000,
-    },
-    "hg38": {
-        "50": 2701495761,
-        "75": 2747877777,
-        "100": 2805636331,
-        "150": 2862010578,
-        "200": 2887553303,
-    },
-    "mm37": {
-        "50": 2304947926,
-        "75": 2404646224,
-        "100": 2462481010,
-        "150": 2489384235,
-        "200": 2513019276,
-    },
-    "mm38": {
-        "50": 2308125349,
-        "75": 2407883318,
-        "100": 2467481108,
-        "150": 2494787188,
-        "200": 2520869189,
-    },
-}
-
-# Read multiqc file to get read length
-# WHY?: Some experiments have multiple read lengths (not ideal...), so we need to get the read length from the multiqc file for each file
-with open(multiqc) as f:
-    lines = f.readlines()
-    line = [line.strip() for line in lines if sample in line][0]
-
-read_length = line.split("\t")[4]
-
-# Get effective genome size
-effective_genome_size = default_effective_genome_size[genome][read_length]
-
-# Correct effective genome size for removal of MT sequences, if applied
-if snakemake.params["remove_MT_seqs"]:
-    # Load MT genome size
-    with open(snakemake.input["mgs"]) as f:
-        mt_genome_size = int(f.read().strip())
-    effective_genome_size -= mt_genome_size
+# Retrieve effective genome size for sample
+with open(egs, 'r') as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader)  # Skip header row
+    for row in csv_reader:
+        sample_name = row[0]
+        if sample_name == sample:
+            effective_genome_size = int(row[1])
+            break
 
 # Run deeptools
 shell(
@@ -92,13 +49,12 @@ shell(
     "--numberOfProcessors {threads} "
     "--effectiveGenomeSize {effective_genome_size} "
     "--bam {bam} "
-    "--scaleFactor {scale_factor} "
+    "{scaling} "
     "--binSize {binsize} "
-    "--normalizeUsing {norm} "
-    "--extendReads "
+    "{normalisation} "
+    #"--extendReads "
     "--outFileName {output} "
     "{extra} "
     "{log} "
 )
-
 
