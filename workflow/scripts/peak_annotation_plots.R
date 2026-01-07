b8ec5cea39b888d25ee05ed839d8166077d07ffb
niekwit <41302859+niekwit@users.noreply.github.com>
2026-01-07 09:45:14 +0000

    Air formatting

--- workflow/scripts/peak_annotation_plots.R
+++ workflow/scripts/peak_annotation_plots.R
@@ -21,58 +21,49 @@ sample_info <- read.csv("config/samples.csv", header = TRUE)
 
 # Add sample names to bed files
 # Check which bed files are provided
-if (any(grepl("\\.narrowPeak$", bed.files)) == TRUE) {
-  samples <- sub(".*\\/([^\\/]+)\\_peaks.narrowPeak", "\\1", bed.files)
-} else if (any(grepl("\\.broadPeak$", bed.files)) == TRUE) {
-  samples <- sub(".*\\/([^\\/]+)\\_peaks.broadPeak", "\\1", bed.files)
-}
-names(bed.files) <- samples
+conditions <- sub(".*\\/([^\\/]+)\\.bed", "\\1", bed.files)
 
-# Get sample condition from sample_info
-conditions <- unique(str_replace(sample_info$sample, "_[0-9]+$", ""))
+names(bed.files) <- conditions
 
-# Order named bed.files so that they match the order of conditions (match to names)
-bed.files <- bed.files[conditions]
-
+print(bed.files)
 # Annotate bed files
-peakAnnoList <- lapply(bed.files,
-                        annotatePeak,
-                        TxDb = txdb,
-                        tssRegion = c(-3000, 3000)
-                        )
+peakAnnoList <- lapply(
+  bed.files,
+  annotatePeak,
+  TxDb = txdb,
+  tssRegion = c(-3000, 3000)
+)
 
 # Plot binding relative to TSS
-pdf(snakemake@output[["dt"]],
-    width = 10,
-    height = length(bed.files) * 2.5)
-plotDistToTSS(peakAnnoList,
-              title =  "Distribution of binding sites relative to TSS") +
-  theme(axis.line.y = element_line(linewidth = 0),
-        axis.line.x = element_line(linewidth = 0.5),
-        axis.ticks.y = element_blank(),
-        panel.border = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.grid.major = element_blank(),
-        text = element_text(size = 20),
-        plot.title = element_text(hjust = 0.5)) +
-  scale_fill_manual(values = viridis(6),
-                    name = "Distance to TSS")
+pdf(snakemake@output[["dt"]], width = 10, height = length(bed.files) * 2.5)
+plotDistToTSS(
+  peakAnnoList,
+  title = "Distribution of binding sites relative to TSS"
+) +
+  theme(
+    axis.line.y = element_line(linewidth = 0),
+    axis.line.x = element_line(linewidth = 0.5),
+    axis.ticks.y = element_blank(),
+    panel.border = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    text = element_text(size = 20),
+    plot.title = element_text(hjust = 0.5)
+  ) +
+  scale_fill_manual(values = viridis(6), name = "Distance to TSS")
 dev.off()
 
 # Plot annotation bar
 pdf(snakemake@output[["fd"]])
-plotAnnoBar(peakAnnoList,
-            title =  "Binding site distribution") +
-  theme(axis.line.y = element_line(linewidth = 0),
-        axis.line.x = element_line(linewidth = 0.5),
-        axis.ticks.y = element_blank(),
-        panel.border = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.grid.major = element_blank(),
-        text = element_text(size = 20),
-        plot.title = element_text(hjust = 0.5))
+plotAnnoBar(peakAnnoList, title = "Binding site distribution") +
+  theme(
+    axis.line.y = element_line(linewidth = 0),
+    axis.line.x = element_line(linewidth = 0.5),
+    axis.ticks.y = element_blank(),
+    panel.border = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    text = element_text(size = 20),
+    plot.title = element_text(hjust = 0.5)
+  )
 dev.off()
-
-# Close redirection of output/messages
-sink(log, type = "output")
-sink(log, type = "message")
--- workflow/scripts/plot_PCA.R
+++ workflow/scripts/plot_PCA.R
@@ -1,5 +1,5 @@
 # Redirect R output to log
-log <- file(snakemake@log[[1]], open="wt")
+log <- file(snakemake@log[[1]], open = "wt")
 sink(log, type = "output")
 sink(log, type = "message")
 
@@ -13,9 +13,7 @@ library(scales)
 
 #### PCA plot ####
 # Load PCA data
-data <- read.delim(snakemake@input[[1]],
-                   header = TRUE,
-                   skip = 1) 
+data <- read.delim(snakemake@input[[1]], header = TRUE, skip = 1)
 colnames(data) <- str_replace(colnames(data), "^X", "")
 
 # Load sample information
@@ -46,40 +44,35 @@ df <- data[1:2, ] %>%
   t() %>%
   as.data.frame() %>%
   mutate(sample = rownames(.)) %>%
-  rename(PC1 = 1,
-         PC2 = 2) %>%
-  mutate(sample_condition = str_replace(sample,
-                                        "_[0-9]+$",
-                                        ""),
-         colour = colours[sample_condition])
+  rename(PC1 = 1, PC2 = 2) %>%
+  mutate(
+    sample_condition = str_replace(sample, "_[0-9]+$", ""),
+    colour = colours[sample_condition]
+  )
 
 # Calculate variance explained for each PC
 PC1_var <- round((data$Eigenvalue[1] / sum(data$Eigenvalue)) * 100, 1)
 PC2_var <- round((data$Eigenvalue[2] / sum(data$Eigenvalue)) * 100, 1)
 
 # Create PCA plot
-p <- ggplot(df,
-            mapping = aes(x = PC1,
-                          y = PC2,
-                          colour = colour)) +
-  geom_point(shape = 19,
-             size = 5) +
-  geom_label_repel(data = df,
-                   aes(label = sample,
-                       fill = NULL),
-                   size = 5,
-                   nudge_x = 0.5,
-                   nudge_y = 0.5) +
+p <- ggplot(df, mapping = aes(x = PC1, y = PC2, colour = colour)) +
+  geom_point(shape = 19, size = 5) +
+  geom_label_repel(
+    data = df,
+    aes(label = sample, fill = NULL),
+    size = 3.5,
+    nudge_x = 0.5,
+    nudge_y = 0.5
+  ) +
   theme_cowplot(16) +
-  labs(x = paste0("PC1: ", PC1_var, "% variance"),
-       y = paste0("PC2: ", PC2_var, "% variance")) +
+  labs(
+    x = paste0("PC1: ", PC1_var, "% variance"),
+    y = paste0("PC2: ", PC2_var, "% variance")
+  ) +
   theme(legend.position = "none")
 
 # Save plot
-ggsave(snakemake@output[["pca"]],
-       p,
-       height = 4,
-       width = 6)
+ggsave(snakemake@output[["pca"]], p, height = 4, width = 6)
 
 #### Scree plot ####
 # Scale factor for utilising whole second y-axis range
@@ -88,43 +81,56 @@ scalefactor <- max(data$Eigenvalue) / 100
 
 # Prepare data for scree plot
 df <- data %>%
-  dplyr::select(c("Component","Eigenvalue")) %>%
+  dplyr::select(c("Component", "Eigenvalue")) %>%
   mutate(Component = paste0("PC", Component)) %>%
-  mutate(cumulative_variance = (cumsum(Eigenvalue) / sum(Eigenvalue) * 100 * scalefactor))
+  mutate(
+    cumulative_variance = (cumsum(Eigenvalue) /
+      sum(Eigenvalue) *
+      100 *
+      scalefactor)
+  )
+
+# Re-level PC factors to ensure correct order
+df$Component <- factor(df$Component, levels = df$Component)
 
 # Create scree plot
 s <- ggplot(df, aes(Component, cumulative_variance)) +
-  geom_bar(aes(Component, Eigenvalue),
-           stat = "identity",
-           colour = "black",
-           fill = "aquamarine4") +
-  geom_line(mapping = aes(x = Component,
-                          y = cumulative_variance,
-                          group = 1),
-            colour = "red",
-            linewidth = 1) +
-  geom_point(mapping = aes(x = Component,
-                           y = cumulative_variance),
-             colour = "red",
-             fill = "white",
-             shape = 21,
-             size = 5,
-             stroke = 1.5) +
+  geom_bar(
+    aes(Component, Eigenvalue),
+    stat = "identity",
+    colour = "black",
+    fill = "aquamarine4"
+  ) +
+  geom_line(
+    mapping = aes(x = Component, y = cumulative_variance, group = 1),
+    colour = "red",
+    linewidth = 1
+  ) +
+  geom_point(
+    mapping = aes(x = Component, y = cumulative_variance),
+    colour = "red",
+    fill = "white",
+    shape = 21,
+    size = 5,
+    stroke = 1.5
+  ) +
   theme_cowplot(15) +
-  theme(axis.title.y.right = element_text(color = "red"),
-        axis.text.y.right = element_text(color = "red")) +
-  scale_y_continuous(sec.axis = sec_axis(transform = ~ .x / scalefactor,
-                                         breaks = seq(0, 100, 25),
-                                         name = "Cumulative variance explained (%)"),
-                     expand = expansion(mult = c(0, .05))) +
-  labs(x = "Principal component",
-       y = "Eigenvalue")
+  theme(
+    axis.title.y.right = element_text(color = "red"),
+    axis.text.y.right = element_text(color = "red")
+  ) +
+  scale_y_continuous(
+    sec.axis = sec_axis(
+      transform = ~ .x / scalefactor,
+      breaks = seq(0, 100, 25),
+      name = "Cumulative variance explained (%)"
+    ),
+    expand = expansion(mult = c(0, .05))
+  ) +
+  labs(x = "Principal component", y = "Eigenvalue")
 
 # Save plot
-ggsave(snakemake@output[["scree"]],
-       s,
-       height = 4,
-       width = 6)
+ggsave(snakemake@output[["scree"]], s, height = 4, width = 6)
 
 # Close redirection of output/messages
 sink(log, type = "output")
