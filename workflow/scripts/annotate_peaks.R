c2be6c1b6c5e6360cb3e591a1a93a7f454edc403
niekwit <41302859+niekwit@users.noreply.github.com>
2026-05-14 11:28:17 +0100

    working

--- workflow/scripts/annotate_peaks.R
+++ workflow/scripts/annotate_peaks.R
@@ -9,63 +9,26 @@ library(ChIPseeker)
 library(tidyverse)
 
 # Load Snakemake parameters
-xls <- snakemake@input[["xls"]]
-DB <- snakemake@input[["adb"]]
+bed_file <- snakemake@input[["bed"]]
+edb_file <- snakemake@input[["edb"]]
 peak.mode <- snakemake@params[["pm"]]
 txt <- snakemake@output[["txt"]]
-bed.file <- snakemake@output[["bed"]]
 gtf <- snakemake@params[["gtf"]]
-
-# Determine how many lines to skip in xls file (comment lines)
-if (peak.mode == "macs2_narrow") {
-  skip <- 21
-} else if (peak.mode == "macs2_broad") {
-  skip <- 22
-} else {
-  stop("Invalid peak mode found...")
-}
-
-# Load xls file and convert to bed
-xls <- read.table(xls,
-                  skip = skip,
-                  header = TRUE)
-bed <- xls %>%
-  select(c("chr", "start", "end", "name", "fold_enrichment")) %>%
-  mutate(strand = ".")
-
-# Write bed to file (annotatePeak requires a file)
-write.table(bed,
-            file = bed.file,
-            quote = FALSE,
-            sep = "\t",
-            row.names = FALSE,
-            col.names = FALSE)
 
 # Load annotation database
 txdb <- makeTxDbFromGFF(gtf)
 
 # Annotate bed file
-peakAnno <- annotatePeak(bed.file,
-                         tssRegion = c(-3000, 3000),
-                         TxDb = txdb
-                        )
+peakAnno <- annotatePeak(bed_file, tssRegion = c(-3000, 3000), TxDb = txdb)
 
 # Tidy up annotation data
 df <- as.data.frame(peakAnno@anno@elementMetadata@listData)
 names(df)[1:3] <- c("peak_id", "fold_enrichment", "strand")
 
 # Add gene names and gene biotype to annotation
-load(DB)
+load(edb_file)
 df <- df %>%
   left_join(edb, by = "geneId")
 
 # Write annotation to file
-write.table(df,
-            file = txt,
-            quote = FALSE,
-            sep = "\t",
-            row.names = FALSE)
-
-# Close redirection of output/messages
-sink(log, type = "output")
-sink(log, type = "message")
+write.table(df, file = txt, quote = FALSE, sep = "\t", row.names = FALSE)
