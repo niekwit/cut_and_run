f30cc7b8f008694f194b0e4ff9f32945b4374705
niekwit <41302859+niekwit@users.noreply.github.com>
2026-05-14 11:29:54 +0100

    Air formatting, still needs more work

--- workflow/scripts/diffbind.R
+++ workflow/scripts/diffbind.R
@@ -3,89 +3,117 @@ log <- file(snakemake@log[[1]], open = "wt")
 sink(log, type = "output")
 sink(log, type = "message")
 
+library(tidyverse)
 library(DiffBind)
 
 # Load Snakemake variables
 bams <- snakemake@input["bam"]
 xls_peaks <- snakemake@input["xls"]
-control_available <- snakemake@params["control"]
+sample_sheet <- snakemake@output["sh"]
+dba_file <- snakemake@output["dba"]
+pca <- snakemake@output["pca"]
+sc <- snakemake@output["sc"]
+pp <- snakemake@output["pp"]
 
 # Load sample information
 samples <- read.csv("config/samples.csv")
 
 # Create combined factor/condition/treatment column to create simple model
-samples$comb <- paste(samples$factor, samples$condition, samples$treatment, sep = "_")
-
-# Create DiffBind sample table
-db.table <- data.frame() %>%
-  mutate(SampleID = samples$sample,
-         Tissue = NA,
-         Factor = samples$factor,
-         Condition = samples$comb,
-         Treatment = samples$treatment,
-         #Replicate = samples$Replicate,
-         bamReads = bams,
-         Peaks = xls_peaks,
-         PeakCaller = "macs")
-
-# Add ControlID and bamControl conditionally
-# and load files from Snakemake
-if (control_available == "True") {
-  db.table <- db.table %>%
-    mutate(ControlID = snakemake@wildcards["control_sample"],
-           bamControl = snakemake@input["control_bam"])
-
-  control.bams <- snakemake@input["control_bam"]
+if (
+  length(unique(samples$genotype)) > 1 & length(unique(samples$treatment)) > 1
+) {
+  samples$comb <- paste(samples$genotype, samples$treatment, sep = "_")
+} else if (
+  length(unique(samples$genotype)) > 1 & length(unique(samples$treatment) == 1)
+) {
+  samples$comb <- samples$genotype
+} else if (
+  length(unique(samples$genotype)) == 1 & length(unique(samples$treatment) > 1)
+) {
+  samples$comb <- samples$treatment
 }
 
-# Add replicate number
-db.table <- db.table %>%
-  group_by(Condition) %>%
-  mutate(Replicate = 1:row_number(.))
+# Create base DiffBind sample table
+db.table <- data.frame(
+  SampleID = samples$sample,
+  Factor = samples$factor,
+  Genotype = samples$genotype,
+  Treatment = samples$treatment,
+  Condition = samples$comb
+)
+db.table$Replicate <- str_split_fixed(samples$sample, "_", 2)[, 2]
 
-print(db.table)
+# Get sample bam files
+for (i in samples$sample) {
+  db.table[db.table$SampleID == i, "bamReads"] <- bams[grep(i, bams)]
+}
+
+# Get control samples and data
+if ("control" %in% names(samples)) {
+  for (i in samples$control) {
+    db.table$ControlID <- samples$control
+    db.table[db.table$ControlID == i, "bamControl"] <- bams[grep(i, bams)]
+  }
+}
+
+# Add peak information
+for (i in samples$sample) {
+  db.table[db.table$SampleID == i, "Peaks"] <- xls_peaks[grep(i, xls_peaks)]
+  db.table$"PeakCaller" <- "macs"
+}
+
+# Remove redundant columns
+if (all(db.table$Condition == samples$genotype)) {
+  db.table$Genotype <- NULL
+} else if (all(db.table$Condition == samples$treatment)) {
+  db.table$Treatment <- NULL
+}
+
+# Get reference conditions
+references <- unique(samples[samples$reference == "yes", "comb"])
+
+# Save sample sheet to file
+write.table(
+  db.table,
+  file = sample_sheet,
+  sep = "\t",
+  quote = FALSE,
+  row.names = FALSE
+)
 
 # Perform DiffBind analysis
-dba <- tamoxifen <- dba(sampleSheet="tamoxifen.csv") %>%
+dba <- dba(sampleSheet = db.table) %>%
   dba.count() %>%
   dba.normalize() %>%
   dba.contrast(minMembers = 2) %>%
-  dba.analyze(design = "~Condition", #simple model for now, implement more complex model later
-              bGreylist = FALSE)
+  dba.analyze(design = ~Condition, bGreylist = FALSE)
 
-# Save diffbind object to file 
-save(dba, file = snakemake@output["dba"])
+# Save diffbind object to file
+save(dba, file = dba_file)
 
-# Plot PCA
-pdf(snakemake@output["pca"])
-dba.plotPCA(dba, DBA_CONDITION, label = DBA_ID)
-dev.off()
-
-# Plot correlation heatmap
-pdf(snakemake@output["sc"])
+# Generate correlation heatmap
+pdf(sc)
 plot(dba)
 dev.off()
 
+# Plot PCA
+pdf(pca)
+dba.plotPCA(dba, DBA_CONDITION, label = DBA_ID)
+dev.off()
+
+# Perform differential binding analysis
+for (i in dba$contrasts) {
+  contrast <- i$contrast
+  print(contrast)
+  #dba.temp <- dba.analyze(dba, contrast = i)
+}
+
+
 # Profile plot
-pdf(snakemake@output["pp"])
+pdf(pp)
 dba.plotProfile(dba, samples = dba$masks$All, doPlot = TRUE)
 dev.off()
 
-# Get all reference conditions
-ref_conditions <- samples[samples$reference == "yes", "comb"]
-if (length(ref_conditions) == 0) {
-  stop("No reference samples found in samples.csv")
-}
-
-# Perform differential binding analysis for each
-# pairwise comparison to each reference conditions
-for (reference in ref_conditions) {
-  # Set control condition
-  diffbind <- dba.contrast(dba, reorderMeta = list(Condition = reference))
-  
-  # Get number of contrasts
-  contrasts <- length(diffbind$contrasts)
-}
 
 # Close redirection of output/messages
 sink(log, type = "output")
